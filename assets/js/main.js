53c8e216b2074caa13fc4b43efbc5a86044de942
Koray <74928882+koraydincc@users.noreply.github.com>
2023-06-07 00:40:21 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,95 +1,77 @@
-const CommonJs = {
-    users: localStorage.getItem('users') 
-        ? JSON.parse(localStorage.getItem("users"))
-        : [],
-
-    loginFormItem: {
-        email: document.getElementById('email'),
-        password: document.getElementById('password'),
-        registerBtn: document.getElementById('registerBtn'),
-    },
+const Commonjs = {
+    users: localStorage.getItem('users')
+    ? JSON.parse(localStorage.getItem('users'))
+    : [],
     registerFormItem: {
         ad: document.getElementById('ad'),
         soyad: document.getElementById('soyad'),
         email: document.getElementById('email'),
-        password: document.getElementById('password'),
+        password: document.getElementById('password')
     },
-
-    GoToPage: function(page) {
-        location.href = page;
+    loginFormItem: {
+        email: document.getElementById('email'),
+        password: document.getElementById('password')
     },
-
-    registerFormCleaner: function() {
-        this.registerFormItem.ad.value = '';
-        this.registerFormItem.soyad.value = '';
-        this.registerFormItem.email.value = '';
-        this.registerFormItem.password.value = '';
-    },
-
-    handleRegisterForm: function() {
+    handleRegisterForm: function () {
+         
         event.preventDefault();
+        
+        let alreadyUser = this.handleAlreadyUser();
+        if (alreadyUser) return false;
 
         this.users.push({
             ad: this.registerFormItem.ad.value,
             soyad: this.registerFormItem.soyad.value,
             email: this.registerFormItem.email.value,
-            password: this.registerFormItem.password.value,
+            password: this.registerFormItem.password.value
+    
         });
-
+        
         this.registerFormCleaner();
-        this.goToPage("../pages/index.html");
+        this.successMessage('Kayıt Başarılı!')
 
         console.log(this.users);
-
-        localStorage.setItem('users', JSON.stringify(this.users));
+        
+        
     },
+    handleLoginForm: function () {
 
-    registerFormValidation: function() {
-        let formItem = [
-            {
-                ad: 'name',
-                message: 'Lütfen isim alanını doldurunuz.',
-            },
-            {
-                soyad: 'soyad',
-                message: 'Soyad alanını doldurunuz',
-            },
-            {
-                email: 'email',
-                message: 'Email alanını doldurunuz',
-            },
-            {
-                password: 'password',
-                message: 'Parola alanını doldurunuz',
-            },
-        ];
-        let formDolumu = true;
-    },
 
-    goToPage: function(page) {
-        localStorage.setItem('users', JSON.stringify(this.users));
-        this.users = JSON.parse(localStorage.getItem('users'));
-        location.href = page;
+        
     },
+    handleAlreadyUser: function () {
+      let alreadyUser = false;
+      
+      this.users.forEach((item, i) => {
+          if (this.registerFormItem.email.value == item.email) {
+            alreadyUser = true
+          }
+        
+      });
+      if (alreadyUser) {
+        this.warningMessage('Bu email ile daha önceden kayıt olunmuştur !')
+        this.registerFormCleaner();
+        return true
+      }
+      return false;
+      
+    },
+    successMessage: function (message) {
+        Swal.fire('Kayıt Başarılı!',message,'success');
+    },
+    warningMessage: function (message) {
+        Swal.fire('Uyarı !',message, 'warning');
+    },
+    registerFormCleaner: function () {
+        this.registerFormItem.ad.value = '',
+        this.registerFormItem.soyad.value = '';
+        this.registerFormItem.email.value = '';
+        this.registerFormItem.password.value = '';
+        
+    }
     
 
-    loginHandle: function() {
-        event.preventDefault();
+    
+}   
 
-        let user = this.users.filter((item) => {
-            return (
-              item.email == this.loginFormItem.email.value &&
-              item.password == this.loginFormItem.password.value
-            );
-        })
-        console.log(this.users)
-    },
-    InitializeEvents: function () {},
-    Init: function () {
-        this.InitializeEvents();
-      },
-};
-window.onload = function () {
-    CommonJs.Init();
-};
-  
+
