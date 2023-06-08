82b104bd804240e8c93febb0db323246c58146be
Koray <74928882+koraydincc@users.noreply.github.com>
2023-06-09 00:33:03 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,8 @@ const Commonjs = {
         ad: document.getElementById('ad'),
         soyad: document.getElementById('soyad'),
         email: document.getElementById('email'),
-        password: document.getElementById('password')
+        password: document.getElementById('password'),
+        repassword: document.getElementById('repassword')
     },
     loginFormItem: {
         email: document.getElementById('email'),
@@ -19,30 +20,58 @@ const Commonjs = {
         let alreadyUser = this.handleAlreadyUser();
         if (alreadyUser) return false;
 
+        
+  
         this.users.push({
             ad: this.registerFormItem.ad.value,
             soyad: this.registerFormItem.soyad.value,
             email: this.registerFormItem.email.value,
-            password: this.registerFormItem.password.value
+            password: this.registerFormItem.password.value,
+            repassword: this.registerFormItem.repassword.value
     
         });
+        //if bloğunun koşulu eğer false ise if bloğu çalışmaz!
+        if (this.confirmPassword()) {
+            return false;
+        }
+
         localStorage.setItem("users", JSON.stringify(this.users));
 
         this.registerFormCleaner();
 
         this.successMessage(`${this.registerFormItem.ad.value} Başarıyla kayıt oldunuz!`);
 
+        console.log(this.users);
+
         setTimeout(() => {
             this.GoToPage("../pages/index.html")
         }, 3000);
 
-        console.log(this.users);
+        
         
         
     },
     handleLoginForm: function () {
-        
 
+      console.log(this.users);
+        
+      event.preventDefault();
+      //filter = Dizideki öğeleri filtreler. Filtrenen öğe varsa geriye döndürür yoksa boş dizi döner
+      // users dizisinin içindeki email ve password filtreledik. Loginformitem ile eşleşirse değer döndürecek
+      let user = this.users.filter(item => {
+        if (
+          item.email == this.loginFormItem.email.value &&
+          item.password == this.loginFormItem.password.value
+        ) {
+          return true;
+        }
+      });
+      
+      if (user.length > 0) {
+        this.successMessage('Sayfaya Yönlendiriliyorsunuz..');
+      } else {
+        this.failEntryMessage('Kullanıcı bulunamadı!');
+      }
         
     },
     handleAlreadyUser: function () {
@@ -62,17 +91,42 @@ const Commonjs = {
       return false;
       
     },
+    confirmPassword: function () {
+
+        const password = this.registerFormItem.password.value;
+        const repassword = this.registerFormItem.repassword.value;
+
+        if (password !== repassword) {
+            this.warningMessage('Şifreniz Doğrulanmadı!')
+            return true;
+            
+        }
+        return false;
+    },
     successMessage: function (message) {
         Swal.fire('Kayıt Başarılı!',message,'success');
     },
     warningMessage: function (message) {
         Swal.fire('Uyarı !',message, 'warning');
     },
+    entryMessage: function (message) {
+        Swal.fire('Giriş Başarılı !', message, 'success')
+    },
+    failEntryMessage: function (message) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: message,
+            footer: '<a href="../pages/register.html">Kayıt olmak için tıklayın.</a>'
+          })
+        
+    },
     registerFormCleaner: function () {
         this.registerFormItem.ad.value = '',
         this.registerFormItem.soyad.value = '';
         this.registerFormItem.email.value = '';
         this.registerFormItem.password.value = '';
+        this.registerFormItem.repassword.value = '';
         
     },
     GoToPage: function (page) {
